3199ad964f41bf81074b439700b2d6a4b75be620
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2022-12-23 16:51:55 +0100

    added score function and corrected create deck

--- classes.py
+++ classes.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, name):
         self._name = name
         self._chips = 10000
+        self._cards = []
 
     @property
     def name(self):
@@ -23,14 +24,8 @@ class Player:
 
 
 class ComputerPlayer(Player):
-    # def make_smart_decision(self, phase):
-    #     cards = []
-    #     for card in self.card:
-    #         cards.append((card.suit, card.rank))
-    #     cards_sorted_by_rank = sorted(cards, key=cards[1])
-    #     cards_sorted_by_suit = sorted(card)
-
-
+    def make_smart_decision(self, phase):
+        pass
 
 
 class HumanPlayer(Player):
@@ -109,8 +104,126 @@ class Game:
 
 def create_deck():
     deck = []
-    suits = ['c', 'd', 'h', 's']
-    for suit in suits:
-        for rank in range(1, 14):
+    """
+    suits:
+        4: spades
+        3: hearts
+        2: diamonds
+        1: Clubs
+    """
+    for suit in range(1, 5):
+        for rank in range(2, 15):
             deck.append(Card(suit, rank))
     return deck
+
+
+def score(players_cards):
+    cards = []
+    for card in players_cards:
+        cards.append((card.suit, card.rank))
+    cards
+    ranks = [0 for i in range(2, 15)]
+    suits = [0 for i in range(1, 5)]
+    cards_sorted_by_rank = sorted(cards, key=lambda x: x[1], reverse=True)
+    counter1 = 0
+    for i in range(1, len(cards_sorted_by_rank)):
+        if cards_sorted_by_rank[i] + 1 == cards_sorted_by_rank[i-1]:
+            counter1 += 1
+            first1 = cards_sorted_by_rank[i-1]
+        else:
+            counter1 = 0
+    pairs = 0
+    threes = 0
+    fours = 0
+    for rank in cards:
+        ranks[rank[1]] += 1
+    for suit in cards:
+        suits[suit[0]] += 1
+    for i in range(1, 5):
+        if suits[i] >= 5:
+            color = i
+    for rank in ranks:
+        if rank == 2:
+            pairs += 1
+        elif rank == 3:
+            threes += 1
+        elif rank == 4:
+            fours += 1
+    score_color = 0
+    if pairs == 0 and threes == 0 and fours == 0:
+        for i in range(14, 1, -1):
+            if ranks[i] == 1:
+                score = i
+                for card in cards_sorted_by_rank:
+                    if card[1] == i:
+                        score_color = max(score_color, card[0])
+                break
+    elif pairs == 1 and threes == 0 and fours == 0:
+        for i in range(14, 1, -1):
+            if ranks[i] == 2:
+                score = 10 * i
+                for card in cards_sorted_by_rank:
+                    if card[1] == i:
+                        score_color = max(score_color, card[0])
+                break
+    elif pairs >= 2 and threes == 0 and fours == 0:
+        h = False
+        low = False
+        for i in range(14, 1, -1):
+            if ranks[i] == 2:
+                if not h:
+                    if i == 14:
+                        score += 1
+                    score += 100 * i
+                    for card in cards_sorted_by_rank:
+                        if card[1] == i:
+                            score_color = max(score_color, card[0])
+                elif not low:
+                    score += 10 * i
+                    low = True
+                    for card in cards_sorted_by_rank:
+                        if card[1] == i:
+                            score_color = max(score_color, card[0])
+                h = True
+    elif pairs == 0 and threes >= 1 and fours == 0:
+        for i in range(14, 1, -1):
+            if ranks[i] == 3:
+                score = 1000 * i
+                for card in cards_sorted_by_rank:
+                    if card[1] == i:
+                        score_color = max(score_color, card[0])
+                break
+    elif pairs >= 1 and threes >= 1 and fours == 0:
+        three = False
+        two = False
+        for i in range(14, 1, -1):
+            if ranks[i] == 3 and not three:
+                score += 1000000 * i
+                three = True
+            elif ranks[i] == 2 and not two:
+                two = True
+                score += 10 * i
+    elif fours:
+        for i in range(14, 1, -1):
+            if ranks[i] == 4:
+                score = i * 10000000
+    elif color:
+        score_color = color
+        chance_for_poker = []
+        for card in cards_sorted_by_rank:
+            if card[0] == color:
+                chance_for_poker.append(card[1])
+        counter = 0
+        for i in range(1, len(chance_for_poker)):
+            if chance_for_poker[i] + 1 == chance_for_poker[i-1]:
+                counter += 1
+                first = chance_for_poker[i-1]
+            else:
+                counter = 0
+        if counter >= 5:
+            score = 150000000 + first
+        else:
+            score = chance_for_poker[0] * 100000
+    elif counter1 >= 5:
+        score = first1 * 10000
+    return (score, score_color)
